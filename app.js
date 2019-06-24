48d9b2e6b7a626c9c7a8f3a00afece653654aa67
root <root@DESKTOP-T1I81SR.localdomain>
2019-06-24 02:33:49 -0300

    Almost there

--- app.js
+++ app.js
@@ -5,12 +5,34 @@ const port = 8080;
 
 const server = http.createServer((req, res) => {
 	let fs = require('fs');
-	let contents = fs.readFileSync('hello_world.html', 'utf8');
-	let name = "Álvaro";
-	let test = "Hello ${name}!";
+	let contents = fs.readFileSync('model/index.html', 'utf-8');
+	
+	let modules = [];
+	modules.style = fs.readFileSync('model/css/index.css', 'utf-8');
+	Object.keys(modules).forEach((e) => {
+		contents = contents.replace("${"+e+"}", modules[e]);
+	})
+	
+	let variables = [];
+	variables.style = fs.readFileSync('model/css/index.css', 'utf-8');
+	Object.keys(variables).forEach((e) => {
+		contents = contents.replace("${"+e+"}", variables[e]);
+	})
+	
+    if (req.method === 'POST') {
+		let body = '';
+		req.on('data', chunk => {
+			body += chunk.toString(); // convert Buffer to string
+		});
+		req.on('end', () => {
+			console.log(body);
+			res.end('ok');
+		});
+	}
+	
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  eval("res.end('" + contents + "')");
+  res.end(contents);
 });
 
 server.listen(port, hostname, () => {
