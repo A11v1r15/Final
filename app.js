0ac3f0ecbc2afd1e48325e780ef724b27fd16d97
Álvaro Carvalho <a11v1r15@Rocketmail.com>
2019-06-24 12:49:37 -0300

    Heroku2

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ const port = 8080;
 
 const server = http.createServer((req, res) => {
 	let fs = require('fs');
-	let contents = fs.readFileSync('model/index.html', 'utf8');
+	let contents = fs.readFileSync('/model/index.html', 'utf8');
 
 	let modules = [];
-	modules.style = fs.readFileSync('model/css/index.css', 'utf-8');
+	modules.style = fs.readFileSync('/model/css/index.css', 'utf-8');
 	modules.numeros = "";
 
 	let variables = [];
@@ -19,20 +19,20 @@ const server = http.createServer((req, res) => {
 	variables.N = "";
 	variables.highest = "";
 
-	if (!(fs.existsSync("sorted.json") && hasJsonStructure(fs.readFileSync("sorted.json", 'utf8')))) {
+	if (!(fs.existsSync("/sorted.json") && hasJsonStructure(fs.readFileSync("/sorted.json", 'utf8')))) {
 		let x = [];
-		fs.writeFile("sorted.json", JSON.stringify(x), 'utf8', () => {
+		fs.writeFile("/sorted.json", JSON.stringify(x), 'utf8', () => {
 			console.log("created sorted")
 		});
 	}
 
-	if (!(fs.existsSync("map.json") && hasJsonStructure(fs.readFileSync("map.json", 'utf8'))) || JSON.parse(fs.readFileSync("sorted.json", 'utf8')).length > 1000) {
+	if (!(fs.existsSync("/map.json") && hasJsonStructure(fs.readFileSync("/map.json", 'utf8'))) || JSON.parse(fs.readFileSync("/sorted.json", 'utf8')).length > 1000) {
 		let x = [];
 		for (let n = 0; n < 100; n++) {
 			x[n] = n;
 		}
 		shuffleArray(x);
-		fs.writeFile("map.json", JSON.stringify(x), 'utf8', () => {
+		fs.writeFile("/map.json", JSON.stringify(x), 'utf8', () => {
 			console.log("created map")
 		});
 	}
@@ -40,33 +40,35 @@ const server = http.createServer((req, res) => {
 	if (req.method === 'POST') {
 		let body = '';
 		req.on('data', function(chunk) {
-		  body += chunk;
+			body += chunk;
 		});
 		req.on('end', function() {
-		  let data = qs.parse(body);
-		console.log(data)
-		
-		modules.numeros = fs.readFileSync('model/numeros.html', 'utf-8');
+			let data = qs.parse(body);
+			console.log(data)
 
-		let map = JSON.parse(fs.readFileSync("map.json", 'utf8'));
-		variables.n1 = data.n1;
-		variables.n2 = data.n2;
-		variables.n3 = data.n3;
-		variables.N = map[variables.n1] * map[variables.n2] * map[variables.n3];
-		
-		let sorted = JSON.parse(fs.readFileSync("sorted.json", 'utf8'));
-		sorted.push(variables.N);
-		sorted = sorted.sort(function(a, b){return b - a});
-		fs.writeFile("sorted.json", JSON.stringify(sorted), 'utf8', () => {
-			console.log("updated sorted")
-		});
-		variables.highest = sorted[0];
-		
-		finishView(res, contents, modules, variables);
+			modules.numeros = fs.readFileSync('/model/numeros.html', 'utf-8');
+
+			let map = JSON.parse(fs.readFileSync("/map.json", 'utf8'));
+			variables.n1 = data.n1;
+			variables.n2 = data.n2;
+			variables.n3 = data.n3;
+			variables.N = map[variables.n1] * map[variables.n2] * map[variables.n3];
+
+			let sorted = JSON.parse(fs.readFileSync("/sorted.json", 'utf8'));
+			sorted.push(variables.N);
+			sorted = sorted.sort(function(a, b) {
+				return b - a
+			});
+			fs.writeFile("/sorted.json", JSON.stringify(sorted), 'utf8', () => {
+				console.log("updated sorted")
+			});
+			variables.highest = sorted[0];
+
+			finishView(res, contents, modules, variables);
 		});
 	} else {
 		finishView(res, contents, modules, variables);
-	}	
+	}
 });
 
 server.listen(port, hostname, () => {
@@ -93,7 +95,7 @@ function hasJsonStructure(str) {
 	}
 }
 
-function finishView(res, contents, modules, variables){
+function finishView(res, contents, modules, variables) {
 	Object.keys(modules).forEach((e) => {
 		contents = contents.replace("&{" + e + "}", modules[e]);
 	})
