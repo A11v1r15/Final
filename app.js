606750743ab2201fdcd7c3c9e8d0cf07b858def7
root <root@DESKTOP-T1I81SR.localdomain>
2019-06-24 12:33:20 -0300

    Finally!

--- app.js
+++ app.js
@@ -1,71 +1,116 @@
 const http = require('http');
+const qs = require('querystring');
 
 const hostname = '127.0.0.1';
 const port = 8080;
 
 const server = http.createServer((req, res) => {
-    let fs = require('fs');
-    let contents = fs.readFileSync('model/index.html', 'utf8');
+	let fs = require('fs');
+	let contents = fs.readFileSync('model/index.html', 'utf8');
 
-    if (req.method === 'POST') {
-        try {
-            if (fs.existsSync("map.json") && hasJsonStructure(fs.readFileSync("map.json", 'utf8'))) {
-                console.log(fs.readFileSync("map.json", 'utf8'));
-            } else {
-            let x = [];
-            for (let n = 0; n < 1000; n++) {
-                x[n] = n;
-            }/*
-            for (let i = x.length - 1; i > 0; i--) {
-                const j = Math.floor(Math.random() * (i++));
-                [x[i], x[j]] = [x[j], x[i]];
-            }*/
-            let q = [];
-            for (let i = 0; i < 10; i++) {
-            	q[i]=[];
-                for (let j = 0; j < 10; j++) {
-                	q[i][j]=[];
-                    for (let k = 0; k < 10; k++) {
-                        q[i][j][k] = x[k * 100 + j * 10 + i];
-                    }
-                }
-            }
-            console.log("salvando?");
-            fs.writeFile("map.json", JSON.stringify(q), 'utf8', () => {});
-        }
-        } catch (err) {
-            console.log(err);
-        } 
-    }
+	let modules = [];
+	modules.style = fs.readFileSync('model/css/index.css', 'utf-8');
+	modules.numeros = "";
 
-    let modules = [];
-    modules.style = fs.readFileSync('model/css/index.css', 'utf-8');
-    Object.keys(modules).forEach((e) => {
-        contents = contents.replace("${" + e + "}", modules[e]);
-    })
+	let variables = [];
+	variables.n1 = "";
+	variables.n2 = "";
+	variables.n3 = "";
+	variables.N = "";
+	variables.highest = "";
 
-    let variables = [];
-    variables.style = fs.readFileSync('model/css/index.css', 'utf-8');
-    Object.keys(variables).forEach((e) => {
-        contents = contents.replace("${" + e + "}", variables[e]);
-    })
+	try {
+//		if (!(fs.existsSync("sorted.json") && hasJsonStructure(fs.readFileSync("sorted.json", 'utf8')))) {
+			let x = [];
+			fs.writeFile("sorted.json", JSON.stringify(x), 'utf8', () => {
+				console.log("created sorted")
+			});
+//		}
+	} catch (err) {
+		console.log(err);
+	}
+	
+	try {
+		if (!(fs.existsSync("map.json") && hasJsonStructure(fs.readFileSync("map.json", 'utf8'))) || JSON.parse(fs.readFileSync("sorted.json", 'utf8')).length > 1000) {
+			let x = [];
+			for (let n = 0; n < 100; n++) {
+				x[n] = n;
+			}
+			shuffleArray(x);
+			fs.writeFile("map.json", JSON.stringify(x), 'utf8', () => {
+				console.log("created map")
+			});
+		}
+	} catch (err) {
+		console.log(err);
+	}
 
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end(contents);
+	if (req.method === 'POST') {
+		let body = '';
+		req.on('data', function(chunk) {
+		  body += chunk;
+		});
+		req.on('end', function() {
+		  let data = qs.parse(body);
+		console.log(data)
+		
+		modules.numeros = fs.readFileSync('model/numeros.html', 'utf-8');
+
+		let map = JSON.parse(fs.readFileSync("map.json", 'utf8'));
+		variables.n1 = data.n1;
+		variables.n2 = data.n2;
+		variables.n3 = data.n3;
+		variables.N = map[variables.n1] * map[variables.n2] * map[variables.n3];
+		
+		let sorted = JSON.parse(fs.readFileSync("sorted.json", 'utf8'));
+		sorted.push(variables.N);
+		sorted = sorted.sort(function(a, b){return b - a});
+		fs.writeFile("sorted.json", JSON.stringify(sorted), 'utf8', () => {
+			console.log("updated sorted")
+		});
+		variables.highest = sorted[0];
+		
+		finishView(res, contents, modules, variables);
+		});
+	} else {
+		finishView(res, contents, modules, variables);
+	}	
 });
 
 server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+	console.log(`Server running at http://${hostname}:${port}/`);
 });
 
+function shuffleArray(array) {
+	for (var i = array.length - 1; i > 0; i--) {
+		var j = Math.floor(Math.random() * (i + 1));
+		var temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
+	}
+}
+
 function hasJsonStructure(str) {
-    if (typeof str !== 'string') return false;
-    try {
-        const result = JSON.parse(str);
-        return Object.prototype.toString.call(result) === '[object Object]' 
-            || Array.isArray(result);
-    } catch (err) {
-        return false;
-    }
+	if (typeof str !== 'string') return false;
+	try {
+		const result = JSON.parse(str);
+		return Object.prototype.toString.call(result) === '[object Object]' ||
+			Array.isArray(result);
+	} catch (err) {
+		return false;
+	}
+}
+
+function finishView(res, contents, modules, variables){
+	Object.keys(modules).forEach((e) => {
+		contents = contents.replace("&{" + e + "}", modules[e]);
+	})
+
+	Object.keys(variables).forEach((e) => {
+		contents = contents.replace("${" + e + "}", variables[e]);
+	})
+
+	res.statusCode = 200;
+	res.setHeader('Content-Type', 'text/html');
+	res.end(contents);
 }
