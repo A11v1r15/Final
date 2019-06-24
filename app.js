b17dc04cf3ab118b3eb8d66a0e5f471418ee7ad0
root <root@DESKTOP-T1I81SR.localdomain>
2019-06-24 00:55:50 -0300

    Failed

--- app.js
+++ app.js
@@ -4,9 +4,13 @@ const hostname = '127.0.0.1';
 const port = 8080;
 
 const server = http.createServer((req, res) => {
+	let fs = require('fs');
+	let contents = fs.readFileSync('hello_world.html', 'utf8');
+	let name = "Álvaro";
+	let test = "Hello ${name}!";
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  res.setHeader('Content-Type', 'text/html');
+  eval("res.end('" + contents + "')");
 });
 
 server.listen(port, hostname, () => {
